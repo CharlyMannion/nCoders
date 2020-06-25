71e6477cef676d84d0ef1862385e46df7ad7c800
Charlotte Mannion <charlottemannion100@gmail.com>
2020-06-25 10:41:26 +0100

    adds correct solution

--- betaKatas/multiplesWithString.js
+++ betaKatas/multiplesWithString.js
@@ -1,15 +1,41 @@
+// function getNumber(number){
+//   if (number%15===0) { return "BOTH" };
+//   if (number%3===0) { return "THREE" };
+//   if (number%5===0) { return "FIVE" };
+//   return number;
+// };
+//
+// function getNumberRange(first, last){
+//   var foo = [];
+//   for (var i = first; i <= last; i++) {
+//     evaluatedNum = getNumber(i);
+//      foo.push(evaluatedNum);
+//   }
+//   return foo;
+// };
+
 function getNumber(number){
-  if (number%15===0) { return "BOTH" };
-  if (number%3===0) { return "THREE" };
-  if (number%5===0) { return "FIVE" };
-  return number;
+    if (number % 3 === 0 && number % 5 === 0) {
+        return "BOTH";
+    } else if (number % 3 === 0) {
+        return "THREE";
+    } else if (number % 5 === 0) {
+        return "FIVE";
+    } else {
+        return number;
+    }
 };
 
 function getNumberRange(first, last){
-  var foo = [];
-  for (var i = first; i <= last; i++) {
-    evaluatedNum = getNumber(i);
-     foo.push(evaluatedNum);
-  }
-  return foo;
+    var list = [];
+    if (first < last) {
+        for (var i = first; i <= last; i++) {
+            list.push(getNumber(i));
+        }
+    } else if (first > last) {
+        for (var i = first; i >= last; i--) {
+            list.push(getNumber(i));
+        }
+    }
+    return list;
 };
