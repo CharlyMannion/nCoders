be926f568895c178c8a312db58165ab98d43a81f
Charlotte Mannion <charlottemannion100@gmail.com>
2020-06-30 12:15:27 +0100

    adds hashed out list filter

--- CodingMeetupSeries/githubAdminsComing.js
+++ CodingMeetupSeries/githubAdminsComing.js
@@ -7,3 +7,8 @@ function findAdmin(list, lang) {
   }
   return adminArr;
 }
+
+
+// function findAdmin(list, lang) {
+//   return list.filter(function(dev){return dev.language == lang && dev.githubAdmin == 'yes'});
+// }
